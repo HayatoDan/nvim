9708d5d6257b21d81ecca5b31e8e6dc8efc3b5c6
hayato dan <dan@test.com>
2025-02-26 11:12:19 +0900

    [update] keymapを一部変更

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,14 +23,31 @@ vim.keymap.set('n', 'Y', 'y$')
 
 
 -- ビジュアルモードで連続ペーストを可能に
-vim.keymap.set('v', 'p', '"_d"+p')
+-- Pでペーストするとレジスタが汚れないので，Pとpを入れ替えておく．
+vim.keymap.set('v', 'p', 'P')
+vim.keymap.set('v', 'P', 'p')
 
 
 
--- <leader>lで文末へ
-vim.keymap.set('n', '<leader>l', 'g$')
-vim.keymap.set('v', '<leader>l', 'g$')
+-- ,と$をを入れ替え
+vim.keymap.set('n', ',', '$')
+vim.keymap.set('v', ',', '$')
+vim.keymap.set('n', '$', ',')
+vim.keymap.set('v', '$', ',')
 
 
 -- <leader><tab>でウインドウフォーカスを移動
 vim.keymap.set('n', '<leader><tab>', '<C-w>w')
+
+-- <space>w で現在バッファの「非スペース文字数」を出力
+vim.keymap.set('n', '<space>w', function()
+  -- 現在のバッファから全行を取得
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  -- 改行を含めずに全行を連結
+  local joined = table.concat(lines, "")
+   -- Vim の substitute() を使って半角スペース (" ") と全角スペース ("　") を除去
+  local sanitized = vim.fn.substitute(joined, '[ 　]', '', 'g') 
+  -- vim.fn.strchars() の第2引数を 1 にして合成文字を1文字としてカウント
+  local count = vim.fn.strcharlen(sanitized)
+  print("Char count (excluding spaces): " .. count)
+end, { silent = true })
