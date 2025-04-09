dd1aad730b5a5fa0e7c45e8e29cdfad719a998af
hayato dan <dan@test.com>
2025-04-09 10:47:43 +0900

    [update] バッファ切り替えを変更

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -3,6 +3,10 @@ local opts = { noremap = true, silent = true }
 map('n', '<A-h>', '<Cmd>BufferPrevious<CR>', opts)
 map('n', '<A-l>', '<Cmd>BufferNext<CR>', opts)
 map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<leader>bh', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<leader>bl', '<Cmd>BufferNext<CR>', opts)
+map('n', '<leader>bc', '<Cmd>BufferClose<CR>', opts)
+map('n', '<leader>bC', '<Cmd>BufferClose!<CR>', opts)
 
 return {
   'romgrk/barbar.nvim',
